0b070c9d45944c86981859c362b4691e9268b23e
mynameisbryan <btuescher13@gmail.com>
2024-07-28 17:34:18 +0200

    new terminal

--- scripts/main.js
+++ scripts/main.js
@@ -1,69 +1,61 @@
 document.addEventListener('DOMContentLoaded', () => {
     const tl = gsap.timeline();
-    tl.to(".green-section", {
-        duration: 2,
-        xPercent: -20,
-        yPercent: -20,
-        ease: "power2.inOut"
-    })
-    .to(".white-section", {
-        duration: 2,
-        xPercent: 20,
-        yPercent: 20,
-        ease: "power2.inOut"
-    }, "<")
-    .to(".logo", {
-        duration: 2,
-        opacity: 1,
-        scale: 1.05,  // subtly scales up the logo to give a feeling of coming forward without much drama
-        ease: "power3.easeOut"  
-    }, "-=1.5")
-    .to(".typing-container", {
-        duration: 2,
-        opacity: 1,
-        maxHeight: "1000px",
-        ease: "power2.inOut"
-    }, "-=1.5");
+    
+    const animateSection = (selector, xPercent, yPercent) => {
+        return {
+            duration: 2,
+            xPercent,
+            yPercent,
+            ease: "power2.inOut"
+        };
+    };
 
-    function typeText() {
-        
-        const text = "Hello and welcome! Booting up the latest in data tech for you. Please stand by — Your data analytics journey will start soon!"
-        let index = 0;
+    tl.to(".green-section", animateSection(null, -20, -20))
+      .to(".white-section", animateSection(null, 20, 20), "<")
+      .to(".logo", {
+          duration: 2,
+          opacity: 1,
+          scale: 1.05,
+          ease: "power3.easeOut"  
+      }, "-=1.5")
+      .to(".typing-container", {
+          duration: 2,
+          opacity: 1,
+          maxHeight: "1000px",
+          ease: "power2.inOut"
+      }, "-=1.5");
+      function typeText() {
+        const text = "Hello and welcome! Booting up the latest in data tech for you. Please stand by — Your data analytics journey will start soon!";
         const typingSpeed = 50;
         const textElement = document.getElementById("typing-text");
         const cursorElement = document.getElementById("cursor");
-        cursorElement.style.opacity = 0; // Ensure cursor is initially hidden
+        let index = 0;
 
-        function addCharacter() {
-            const keystrokeSound = new Audio('keystroke.mp3'); // Ensure the path to sound file is correct
+        const addCharacter = () => {
             if (index < text.length) {
-                const span = document.createElement('span');
-                span.textContent = text.charAt(index);
-                span.className = 'visible';
-                textElement.appendChild(span);
-                keystrokeSound.play();
+                textElement.textContent += text.charAt(index);
                 index++;
                 setTimeout(addCharacter, typingSpeed);
             } else {
-                // Start blinking cursor after typing is done
                 cursorElement.style.opacity = 1;
                 blinkCursor();
             }
-        }
+        };
 
-        function blinkCursor() {
+        const blinkCursor = () => {
             setInterval(() => {
-                cursorElement.style.opacity = (cursorElement.style.opacity == 0 ? 1 : 0);
+                cursorElement.style.opacity = cursorElement.style.opacity === '0' ? '1' : '0';
             }, 500);
-        }
+        };
 
         setTimeout(addCharacter, 2500);
     }
 
-    setTimeout(() => {
+    const changeBackgroundColor = () => {
         document.body.style.transition = 'background-color 2s';
-        document.body.style.backgroundColor = '#072c71'; // Example color, change as needed
-    }, 1000);
+        document.body.style.backgroundColor = '#072c71';
+    };
 
+    setTimeout(changeBackgroundColor, 1000);
     typeText();
 });
